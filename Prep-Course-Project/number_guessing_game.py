a66fd9c7b2f82ef7596e62157ed7c21ac4639bf6
gabriel <groisenberg79@gmail.com>
2025-12-08 16:18:00 +0200

    Add initial prompt in guessing game

---
+++ Prep-Course-Project/number_guessing_game.py
@@ -0,0 +1,14 @@
+""" Build a fun Number Guessing Game in Python! 🐍 The program picks a random number between 1-100, and you have 7 attempts to guess it. Get hints if you’re too high 📈 or too low 📉! Perfect for practicing loops 🔄, conditionals ❓, and user input ⌨️.
+ """
+
+print("Welcome to the 'Guess the Number' Game!")
+print()
+
+print("This is how the game works: \n"
+    "- I will pick a random integer between 1 and 100 and you have to guess which number it is.\n"
+    "- Each time you pick a wrong guess, I will give you hints (whether is is higher or lower).\n"
+    "- You will have 7 attempts until the game ends (and you lose).\n"
+    "- Find the number in any of the 7 attempts, and you win!")
+
+
+
